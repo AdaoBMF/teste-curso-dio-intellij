8ce14a192744bd4122c99b5d094c548ad3b1404e
AdaoBMF <adaobmf79@gmail.com>
2021-11-07 10:08:42 -0300

    add class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,9 +8,44 @@ public class PrimeiroPrograma {
     int gatoIdade = gato.getIdade();
     System.out.println(gatoIdade);
 
+    Livro livro1 = new Livro("portões de fogo", 350);
+    System.out.println(livro1);
     /* int a = 19;
     int b = 7;
     System.out.println("hello world!!" + (a + b));*/
 
+  }
+}
+class Livro {
+  private String nome;
+  private Integer numPaginas;
+
+  public Livro(String nome, Integer numPaginas) {
+    this.nome = nome;
+    this.numPaginas = numPaginas;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Integer getNumPaginas() {
+    return numPaginas;
+  }
+
+  public void setNumPaginas(Integer numPaginas) {
+    this.numPaginas = numPaginas;
+  }
+
+  @Override
+  public String toString() {
+    return "Livro{" +
+            "nome='" + nome + '\'' +
+            ", numPaginas=" + numPaginas +
+            '}';
   }
 }
